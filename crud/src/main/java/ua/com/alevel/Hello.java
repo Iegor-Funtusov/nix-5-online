4e28f6dce34ee3f6182c8b9a9c778770db72b3bc
Iegor-Funtusov <funtushan@gmail.com>
2021-04-19 21:15:12 +0300

    added user
    added user

--- crud/src/main/java/ua/com/alevel/Hello.java
+++ crud/src/main/java/ua/com/alevel/Hello.java
@@ -1,11 +1,8 @@
 package ua.com.alevel;
 
-import ua.com.alevel.test.PrintDemo;
-
 public class Hello {
 
     public static void main(String[] args) {
-        PrintDemo printDemo = new PrintDemo();
-        printDemo.print();
+
     }
 }
---
+++ crud/src/main/java/ua/com/alevel/User.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String id;
+    private String name;
+    private String age;
+}
